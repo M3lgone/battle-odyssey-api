2cf2a6e3f5d6a4345311f2cf4bcbad5a23365d68
IsmaelG <ismaelgn89@gmail.com>
2026-06-29 22:19:19 +0200

    feat: add user registration endpoint with tdd

---
+++ tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+it('registers a user', function () {
+
+    $data = [
+        'name' => 'Ismael',
+        'email' => 'isma@gmail.com',
+        'password' => 'password4',
+        'password_confirmation' => 'password4',
+    ];
+
+    $response = $this->postJson('/api/v1/auth/register', $data);
+
+    $response->assertCreated();
+});

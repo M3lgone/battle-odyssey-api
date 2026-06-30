9178f932fa7d1cb44341c5f8af8aac545ccb9575
IsmaelG <ismaelgn89@gmail.com>
2026-06-30 22:18:19 +0200

    feat: add route and controller login

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 Route::prefix('v1')->group(function () {
     Route::post('/auth/register', RegisterController::class);
+    Route::post('/auth/login', LoginController::class);
 });

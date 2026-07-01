2462b44d055ac026f032cc960ded294630fcf05a
IsmaelG <ismaelgn89@gmail.com>
2026-07-01 13:23:42 +0200

    test: add test sad path to login test

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -21,6 +21,31 @@ it('user login and return bearer token', function () {
 
     $response = $this->postJson('/api/v1/auth/login', $data);
 
-    $response->assertOk();
+    $response->assertStatus(200);
     $response->assertJsonStructure(['token']);
+});
+
+it('fails to login with incorrect password', function () {
+
+    $user = User::factory()->create([
+        'password' => Hash::make('password4'),
+    ]);
+
+    $data = [
+        'email' => $user->email,
+        'password' => 'false_password' 
+    ];
+
+    $response = $this->postJson('/api/v1/auth/login', $data);
+
+    $response->assertStatus(401)
+             ->assertJson(['message' => 'Invalid credentials']);
+});
+
+it('fails validation if email or password are missing', function () {
+
+    $response = $this->postJson('/api/v1/auth/login', []);
+
+    $response->assertStatus(422)
+             ->assertJsonValidationErrors(['email', 'password']);
 });

ff88303174e5157d92d06150c0c9ddd49f9f739d
IsmaelG <ismaelgn89@gmail.com>
2026-07-01 23:46:10 +0200

    feat: add route and controller logout

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
 
 Route::prefix('v1')->group(function () {
     Route::post('/auth/register', RegisterController::class);
     Route::post('/auth/login', LoginController::class);
+    
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/auth/logout', LogoutController::class);    
+    });
 });

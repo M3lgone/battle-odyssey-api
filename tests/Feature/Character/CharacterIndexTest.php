346c446e59cb76eb8bf7b785ca0548e1708412fc
IsmaelG <ismaelgn89@gmail.com>
2026-07-06 19:32:09 +0200

    test: add characters test

---
+++ tests/Feature/Character/CharacterIndexTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\User;
+use App\Models\Character;
+use Illuminate\Support\Facades\Artisan;
+use Laravel\Passport\Passport;
+
+beforeEach(function () {
+    Artisan::call('passport:client', ['--personal' => true, '--no-interaction' => true]);
+});
+
+it('player can list all characters', function () {
+
+    $user = User::factory()->create();
+
+    Passport::actingAs($user);
+
+    Artisan::call('db:seed', ['--class' => 'CharacterSeeder']);
+
+    $response = $this->getJson('/api/v1/characters');
+
+    $response->assertStatus(200)
+             ->assertJsonStructure([['id', 'class', 'attack', 'defense', 'max_health_points', 'max_magic_points']]);
+});

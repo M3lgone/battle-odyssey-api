3225b2870af40cff2c2c1bb233e0c7ec32cdf804
IsmaelG <ismaelgn89@gmail.com>
2026-07-06 19:33:09 +0200

    feat: add characters index route

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,11 @@ use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\Admin\AdminUserController;
+use App\Http\Controllers\Api\CharacterController;
 
-Route::prefix('v1')->group(function () {
-    Route::post('/auth/register', RegisterController::class);
-    Route::post('/auth/login', LoginController::class);
+    Route::prefix('v1')->group(function () {
+        Route::post('/auth/register', RegisterController::class);
+        Route::post('/auth/login', LoginController::class);
     
     Route::middleware('auth:api')->group(function () {
         Route::get('/users/{user}', [UserController::class, 'show']);
@@ -19,9 +20,11 @@ Route::prefix('v1')->group(function () {
     });
 
     Route::middleware(['auth:api', 'role:admin'])->prefix('admin')->group(function () {
-    Route::get('/users', [AdminUserController::class, 'index']);
-    Route::get('/users/{user}', [AdminUserController::class, 'show']);
-    Route::patch('/users/{user}', [AdminUserController::class, 'update']);
-    Route::delete('/users/{user}', [AdminUserController::class, 'destroy']);
+        Route::get('/users', [AdminUserController::class, 'index']);
+        Route::get('/users/{user}', [AdminUserController::class, 'show']);
+        Route::patch('/users/{user}', [AdminUserController::class, 'update']);
+        Route::delete('/users/{user}', [AdminUserController::class, 'destroy']);
     });
+
+    Route::get('/characters', [CharacterController::class, 'index']);
 });

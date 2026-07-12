3c9eaf16cc2eb808238f5aab77bcf41808093ce0
IsmaelG <ismaelgn89@gmail.com>
2026-07-12 22:49:23 +0200

    feat: add migrations enemies and enemy has skill

---
+++ database/migrations/2026_07_11_172836_create_enemies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enemies', function (Blueprint $table) {
+            $table->id();
+            $table->string('enemy_name', 45);
+            $table->integer('max_health_points');
+            $table->integer('max_magic_points');
+            $table->integer('attack');
+            $table->integer('defense');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enemies');
+    }
+};
---
+++ database/migrations/2026_07_11_173548_create_enemy_has_skill_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enemy_has_skill', function (Blueprint $table) {
+            $table->foreignId('enemy_id')->constrained('enemies');
+            $table->foreignId('skill_id')->constrained('skills');
+            $table->primary(['enemy_id', 'skill_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enemy_has_skill');
+    }
+};

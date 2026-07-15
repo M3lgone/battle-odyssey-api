a5271d2811f1a4702b6d513d5cdf20e9ba32d579
IsmaelG <ismaelgn89@gmail.com>
2026-07-15 15:34:21 +0200

    feat: add migration and seeder games

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,6 +36,7 @@ class DatabaseSeeder extends Seeder
             CharacterHasSkillSeeder::class,
             EnemySeeder::class,
             EnemyHasSkillSeeder::class,
+            GameSeeder::class,
 
         ]);
     }
